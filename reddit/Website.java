31323cd287ebd3979fa8ecf874f919cc676497da
Japheth Wong <japhethjwong@gmail.com>
2014-02-06 20:47:17 -0800

    Adding methods for importing and exporting data.

--- reddit/Website.java
+++ reddit/Website.java
@@ -1,13 +1,32 @@
 package reddit;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Website {
     public Website () {
 
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        
     }
 
+    /**
+     *  importData() imports data from a file into the program's memory for use.
+     **/
+    private void importData() {
+
+    }
+
+    /**
+     *  exportData() exports data from a file into the program's memory to be loaded 
+     *  in for the next session.
+     **/
+    private void exportData() {
+
+    }
 
 }
