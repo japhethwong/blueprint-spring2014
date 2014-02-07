aec9c3e56570654f0c936314e460dd4801c2fd8a
Japheth Wong <japhethjwong@gmail.com>
2014-02-06 21:09:49 -0800

    Adding importData() for Website.

--- reddit/Website.java
+++ reddit/Website.java
@@ -1,9 +1,13 @@
 package reddit;
 
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+//  CSV Parse library: http://opencsv.sourceforge.net/
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 public class Website {
     public Website () {
@@ -18,6 +22,37 @@ public class Website {
      *  importData() imports data from a file into the program's memory for use.
      **/
     private void importData() {
+        String articlesLoc = "articles.csv";
+        String usersLoc = "users.csv";
+        String communityLoc = "communities.csv";
+
+        //  Read in article data.
+        try {
+            CSVReader articlesReader = new CSVReader(new FileReader(articlesLoc));
+            List articles = articlesReader.readAll();
+        } catch (IOException e) {
+            System.out.println("***ERROR: Unable to locate articles data file.");
+            System.exit(1);
+        }
+        
+        //  Read in user data.
+        try {
+            CSVReader usersReader = new CSVReader(new FileReader(usersLoc));
+            List users = usersReader.readAll();
+        } catch (IOException e) {
+            System.out.println("***ERROR: Unable to locate articles data file.");
+            System.exit(1);   
+        }
+
+        //  Read in communities data.
+        try {
+            CSVReader communitiesReader = new CSVReader(new FileReader(communityLoc));
+            List communities = communities.readAll();
+        } catch (IOException e) {
+            System.out.println("***ERROR: Unable to locate articles data file.");
+            System.exit(1);
+        }
+
 
     }
 
