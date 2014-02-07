06b552f7b648852f1fa37e8a575de96ae7feb062
Gavin Chu <gavincca@gmail.com>
2014-02-06 21:05:12 -0800

    getArticles and getUserArticles

--- reddit/Website.java
+++ reddit/Website.java
@@ -6,14 +6,39 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Website {
+	
+	ArrayList<Community> communities;
+	HashMap<String, User> users;
+	ArrayList<Article> articles;
+	
     public Website () {
 
     }
 	
 	public String getArticles(){
+		StringBuffer sb = new StringBuffer();
+		for(int i = articles.size(); i > articles.size()-10; i--){
+			sb.append(articles.get(i).toString()+ "\n");
+		}
+		return sb.toString();
 	}
 
-	public String getUserArticles(){
+	public String getUserArticles(String user){
+		User currentUser;
+		if(users.contains(user)){
+			currentUser = users.get(user);
+		}else{
+			return "User Not Found";
+		}
+		//User class should implement getArticles()
+		Article[] userArticles = currentUser.getArticles();
+		
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < userArticles.length; i++){
+			sb.append(i + ". " + aricles.get(i).toString()+ "\n");
+		}
+		return sb.toString();
+		
 	}
 
 	public String searchArticles(){
