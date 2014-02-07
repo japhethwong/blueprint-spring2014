f837b16a163b2460b2b418e47ce10f3da9979dae
Gavin Chu <gavincca@gmail.com>
2014-02-06 21:51:59 -0800

    gavin

--- reddit/Website.java
+++ reddit/Website.java
@@ -1,6 +1,7 @@
 package reddit;
 
 // import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,6 +15,8 @@ public class Website {
 	private HashMap<Community> communities;
 	private HashMap<String, User> users;
 	private ArrayList<Article> articles;
+	
+	private final String ERROR = "INVALID COMMAND";
 	
     public Website () {
 
@@ -63,23 +66,81 @@ public class Website {
 		
 	}
 
-	public void postArticle(String user, String content){
-		ArrayList<Community> tags = new ArrayList<Community>();
-		
+	public void postArticle(String user, ArrayList<Community> tags, String content){		
 		Article article = new Article(user, tags, content);
-		
 	}
 
 	public String getUsers(){
 		StringBuffer sb = new StringBuffer();
 		ArrayList<User> allUsers = users.values();
 		for(int i = 0; i < allUsers.size(); i++){
-			sb.append(i + ". " + allUsers.get(i).getUserame();
+			sb.append(i + ". " + allUsers.get(i).getUserame()+ "\n");
 		}
+		return sb.toString();
 	}
 
     public static void main(String[] args) {
-    	
+    	if(args.length > 0){
+    		String command = args[0];
+    		switch (command){
+    		case "listusers":
+    			System.out.println(getUsers());
+    			break;
+    		case "listarticles":
+    			System.out.println(getArticles());
+    			break;
+    		case "listuserarticles":
+    			if(args.length >= 2){
+    				//args[1] contains the username
+    				System.out.println(getUserArticles(args[1]));
+    			}else{
+    				System.out.println(ERROR);
+    			}
+    			break;
+    		case "searcharticles":
+    			if(args.length >= 2){
+    				//args[1] contains the community name
+    				System.out.println(searchArticles(args[1]));
+    			}else{
+    				System.out.println(ERROR);
+    			}
+    			break;
+    		case "post":
+    			if(args.length >= 3){
+    				//args[1] contains the username
+    				ArrayList<Community> tags = new ArrayList<Community>();
+    				System.out.print("Enter a tag (enter empty tag to exit): ");
+    				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    				String tag = null;
+    			    try {
+    			         tag = br.readLine();
+    			         while(tag != null){
+    			        	 if(tag.length() == 0){
+    			        		 break;
+    			        	 }
+	    			         if(communities.contains(tag)){
+	    			        	 tags.add(communities.get(tag));
+	    			         }else{
+	    			        	 System.out.println("Community does not exists.");
+	    			        	 System.out.print("Enter a tag: ");
+	    			         }
+	    			         tag = br.readLine();
+    			         }
+    			    } catch (IOException ioe) {
+    			         System.out.println("IO error trying to read your name!");
+    			    }
+    			    
+    				if(communities.contains())
+    			}else{
+    				System.out.println(ERROR);
+    			}
+    			break;
+			default:
+				System.out.println(ERROR);
+				break;
+    		}
+    	}
         
     }
 
